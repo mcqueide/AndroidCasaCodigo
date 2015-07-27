7872f3cf9541a8f9e0896c8965fed5f33e10c3ba
john.clemente <john.clemente@johnclemente.foton.local>
2015-07-27 12:55:48 -0300

    Fim do Hello World 2, E término do capítulo 1.

--- src/br/com/casaCodigo/helloandroid/MainActivity.java
+++ src/br/com/casaCodigo/helloandroid/MainActivity.java
@@ -2,13 +2,32 @@ package br.com.casaCodigo.helloandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+	private EditText nomeEditText;
+	private TextView saudacaoTextView;
+	private String saudacao;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		this.nomeEditText = (EditText) findViewById(R.id.nomeEditText);
+		this.saudacaoTextView = (TextView) findViewById(R.id.saudacaoTextView);
+		this.saudacao = getResources().getString(R.string.saudacao);
+	}
+
+	public void surpreenderUsuario(View v) {
+		Editable texto = this.nomeEditText.getText();
+		String msg = saudacao + " " + texto.toString();
+		this.saudacaoTextView.setText(msg);
+
 	}
 
 }
