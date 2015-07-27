4fc371ab25b47b6a762fd22c4a962aae5b7cd367
john.clemente <john.clemente@johnclemente.foton.local>
2015-07-27 13:27:46 -0300

    Começo da parte de intent

---
+++ src/br/com/casadocodigo/helloandroid/SaudacaoActivity.java
@@ -0,0 +1,15 @@
+package br.com.casadocodigo.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SaudacaoActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+	
+}
